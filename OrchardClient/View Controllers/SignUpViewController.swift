fe968e710b9e70e06a34d7ec78b3d5df00cb64c3
matthewharrilal <matthewharrilal@gmail.com>
2017-10-26 21:05:24 -0700

    Writing the stages of the sign up flow

---
+++ OrchardClient/View Controllers/SignUpViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpViewController.swift
+//  OrchardClient
+//
+//  Created by Matthew Harrilal on 10/26/17.
+//  Copyright © 2017 Matthew Harrilal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SignUpViewController: UIViewController {
+    //    MARK: UIElements
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
