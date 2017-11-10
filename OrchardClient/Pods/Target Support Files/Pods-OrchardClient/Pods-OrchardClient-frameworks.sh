d2b0aad1e703dca5cdee7dec6d59d68f0303388a
matthewharrilal <matthewharrilal@gmail.com>
2017-11-10 10:35:03 -0800

    Most recent push

--- OrchardClient/Pods/Target Support Files/Pods-OrchardClient/Pods-OrchardClient-frameworks.sh
+++ OrchardClient/Pods/Target Support Files/Pods-OrchardClient/Pods-OrchardClient-frameworks.sh
@@ -103,10 +103,12 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/KeychainSwift/KeychainSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SHSearchBar/SHSearchBar.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Zip/Zip.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/KeychainSwift/KeychainSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SHSearchBar/SHSearchBar.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Zip/Zip.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
