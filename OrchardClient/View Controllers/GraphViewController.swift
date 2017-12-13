51e6c0a9bc300a8d7e47632defbe9c7114b24811
matthewharrilal <matthewharrilal@gmail.com>
2017-12-12 18:40:17 -0800

    Working on the grapghs

--- OrchardClient/View Controllers/GraphViewController.swift
+++ OrchardClient/View Controllers/GraphViewController.swift
@@ -22,6 +22,19 @@ class GraphViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
+//        let githubCommits = GithubCommitsNetworkingLayer()
+//        RepositoryName.repositoryName = self.userRepoName!
+//        githubCommits.network(route: .user(), requestRoute: .get) { (data) in
+//            guard let commits = try? JSONDecoder().decode(Commits.self, from: data) else {return}
+//            print(commits)
+//            self.commitArray = commits.all!
+//        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is the commit array \(commitArray)")
+        
         let githubCommits = GithubCommitsNetworkingLayer()
         RepositoryName.repositoryName = self.userRepoName!
         githubCommits.network(route: .user(), requestRoute: .get) { (data) in
@@ -29,22 +42,7 @@ class GraphViewController: UIViewController {
             print(commits)
             self.commitArray = commits.all!
         }
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print("This is the commit array \(commitArray)")
       
-        for i in commitArray {
-            let value = ChartDataEntry(x: Double(i), y: Double(i))
-            lineChart.append(value)
-        }
-        
-        let line1 = LineChartDataSet(values: lineChart, label: "Number")
-        line1.colors = [UIColor.blue]
-        data.addDataSet(line1)
-        chartView.data = data
-        chartView.chartDescription?.text = "My awesome Graph"
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -54,5 +52,18 @@ class GraphViewController: UIViewController {
     
     var lineChart = [ChartDataEntry]()
     
+    @IBAction func updateGraph(_ sender: Any) {
+        for i in 0 ... (commitArray.count - 1) {
+            print("HTis is i \(i)")
+            let value = ChartDataEntry(x: Double(i), y: Double(commitArray[i]))
+            lineChart.append(value)
+        }
+        
+        let line1 = LineChartDataSet(values: lineChart, label: "Number")
+        line1.colors = [UIColor.blue]
+        data.addDataSet(line1)
+        chartView.data = data
+        chartView.chartDescription?.text = "My awesome Graph"
+    }
     
 }
